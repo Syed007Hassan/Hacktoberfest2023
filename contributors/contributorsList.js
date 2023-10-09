eb25ef793a99c30210a64c721d5020ece5f8fb41
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-10-09 19:31:16 +0500

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+    {
+    id: 435,
+    fullname: "Syed Hassan",
+    username: "https://github.com/Syed007Hassan",
+  },
 ];
